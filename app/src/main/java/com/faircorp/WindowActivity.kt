937a770d9128b97beee8f6ffe7d4cba838b8d5fb
Maxence Caille <caille.maxence@outlook.fr>
2021-10-20 16:16:24 +0200

    j'arrive à faire passer les données

--- app/src/main/java/com/faircorp/WindowActivity.kt
+++ app/src/main/java/com/faircorp/WindowActivity.kt
@@ -14,13 +14,25 @@ class WindowActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_window)
         val id = intent.getLongExtra(WINDOW_NAME_PARAM, 0)
-        //val window = windowService.findById(id)
 
-
+        val windowService = WindowService()
         val param = intent.getStringExtra(WINDOW_NAME_PARAM)
+        // val windowName = findViewById<TextView>(R.id.inputUser)
+        // windowName.text = param ?: id.toString()
+        val window = windowService.findById(id)
 
-        val windowName = findViewById<TextView>(R.id.inputUser)
-        windowName.text = param ?: id.toString()
+        if (window != null) {
+            findViewById<TextView>(R.id.txt_window_name).text = window.name
+            findViewById<TextView>(R.id.txt_room_name).text = window.room.name
+            findViewById<TextView>(R.id.txt_window_current_temperature).text = window.room.currentTemperature?.toString()
+            findViewById<TextView>(R.id.txt_window_target_temperature).text = window.room.targetTemperature?.toString()
+            findViewById<TextView>(R.id.txt_window_status).text = window.status.toString()
+        }
+
+
+
+
+
     }
 
 
