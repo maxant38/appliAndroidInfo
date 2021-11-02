f6e4d86eb48730a5f8fdbebf09f179cf12249c9f
Maxence Caille <caille.maxence@outlook.fr>
2021-11-02 18:01:19 +0100

    Tout marche à reprendred 'ici !'

--- app/src/main/java/com/faircorp/model/WindowDto.kt
+++ app/src/main/java/com/faircorp/model/WindowDto.kt
@@ -1,6 +1,7 @@
 package com.faircorp.model
 
 
-    enum class Status { OPEN, CLOSED}
 
-    data class WindowDto(val id: Long, val name: String, val room: RoomDto, val status: Status)
+enum class Status { OPEN, CLOSED}
+
+data class WindowDto(val id: Long, val name: String, val room: RoomDto, val status: Status)
